fdef62e9c8bc9aa618713b97ef979e0e16890cbe
Auriwave <auriwave@gmail.com>
2023-04-17 16:27:53 +0200

    Fixes ZenithStaff too

--- Items/Weapons/ZenithGun/ZenithGun.cs
+++ Items/Weapons/ZenithGun/ZenithGun.cs
@@ -1,7 +1,6 @@
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
--- Items/Weapons/ZenithStaff/ZenithStaff.cs
+++ Items/Weapons/ZenithStaff/ZenithStaff.cs
@@ -1,76 +1,64 @@
-﻿using log4net.DateFormatter;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using System;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
-namespace ZenithGun.Items.Weapons.ZenithStaff
-{
-	public class ZenithStaffMinionBuff : ModBuff
-	{
-		public override void SetDefaults()
-		{
+
+namespace ZenithGun.Items.Weapons.ZenithStaff {
+	public class ZenithStaffMinionBuff : ModBuff {
+		public override void SetStaticDefaults() {
 			DisplayName.SetDefault("Zenith Staff Minion");
 			Description.SetDefault("The example minion will fight for you");
 			Main.buffNoSave[Type] = true;
 			Main.buffNoTimeDisplay[Type] = true;
 		}
 
-		public override void Update(Player player, ref int buffIndex)
-		{
-			if (player.ownedProjectileCounts[ProjectileType<ZenithStaffMinion>()] > 0)
-			{
+		public override void Update(Player player, ref int buffIndex) {
+			if (player.ownedProjectileCounts[ProjectileType<ZenithStaffMinion>()] > 0) {
 				player.buffTime[buffIndex] = 18000;
-			}
-			else
-			{
+			} else {
 				player.DelBuff(buffIndex);
 				buffIndex--;
 			}
 		}
 	}
 
-	public class ZenithStaffItem : ModItem
-	{
-		public override void SetStaticDefaults()
-		{
+	public class ZenithStaffItem : ModItem {
+		public override void SetStaticDefaults() {
 			DisplayName.SetDefault("Zenith Staff");
 			Tooltip.SetDefault("Summons an example minion to fight for you\nUses 11 minion slots");
-			ItemID.Sets.GamepadWholeScreenUseRange[item.type] = true; // This lets the player target anywhere on the whole screen while using a controller.
-			ItemID.Sets.LockOnIgnoresCollision[item.type] = true;
-			ItemID.Sets.StaffMinionSlotsRequired[item.type] = 1;
+			ItemID.Sets.GamepadWholeScreenUseRange[Item.type] = true; // This lets the player target anywhere on the whole screen while using a controller.
+			ItemID.Sets.LockOnIgnoresCollision[Item.type] = true;
+			ItemID.Sets.StaffMinionSlotsRequired[Item.type] = 1;
 		}
 
-		public override void SetDefaults()
-		{
-			item.damage = 200;
-			item.knockBack = 6f;
-			item.mana = 20;
-			item.width = 52;
-			item.height = 52;
-			item.useTime = 36;
-			item.useAnimation = 36;
-			item.useStyle = ItemUseStyleID.SwingThrow;
-			item.value = Item.buyPrice(0, 30, 0, 0);
-			item.rare = ItemRarityID.Red;
-			item.UseSound = SoundID.Item44;
+		public override void SetDefaults() {
+			Item.damage = 200;
+			Item.knockBack = 6f;
+			Item.mana = 20;
+			Item.width = 52;
+			Item.height = 52;
+			Item.useTime = 36;
+			Item.useAnimation = 36;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.value = Item.buyPrice(0, 30, 0, 0);
+			Item.rare = ItemRarityID.Red;
+			Item.UseSound = SoundID.Item44;
 
 			// These below are needed for a minion weapon
-			item.noMelee = true;
-			item.summon = true;
-			item.buffType = BuffType<ZenithStaffMinionBuff>();
+			Item.DamageType = DamageClass.Summon;
+			Item.buffType = BuffType<ZenithStaffMinionBuff>();
 			// No buffTime because otherwise the item tooltip would say something like "1 minute duration"
-			item.shoot = ProjectileType<ZenithStaffMinion>();
+			Item.shoot = ProjectileType<ZenithStaffMinion>();
 		}
 
-		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
 			// This is needed so the buff that keeps your minion alive and allows you to despawn it properly applies
-			if (player.ownedProjectileCounts[ProjectileType<ZenithStaffMinion>()] == 0)
-			{
-				player.AddBuff(item.buffType, 2);
+			if (player.ownedProjectileCounts[ProjectileType<ZenithStaffMinion>()] == 0) {
+				player.AddBuff(Item.buffType, 2);
 			}
 
 			// Here you can change where the minion is spawned. Most vanilla minions spawn at the cursor position.
@@ -78,13 +66,11 @@ namespace ZenithGun.Items.Weapons.ZenithStaff
 			return true;
 		}
 
-		public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 1);
-			recipe.AddTile(TileID.LunarCraftingStation);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
+		public override void AddRecipes() {
+			Recipe baseRecipe = CreateRecipe()
+				.AddTile(TileID.LunarCraftingStation)
+				.AddIngredient(ItemID.DirtBlock, 1)
+				.Register();
 		}
 	}
 
@@ -94,81 +80,72 @@ namespace ZenithGun.Items.Weapons.ZenithStaff
 	 * If the player targets a certain NPC with right-click, it will fly through tiles to it
 	 * If it isn't attacking, it will float near the player with minimal movement
 	 */
-	public class ZenithStaffMinion : ModProjectile
-	{
+	public class ZenithStaffMinion : ModProjectile {
 		Random random;
 		double theta = -1;
 		bool isSpinning, isSlashing = false;
 		Vector2 direction;
 
-		public override void SetStaticDefaults()
-		{
+		public override void SetStaticDefaults() {
 			DisplayName.SetDefault("Zenith Minion");
 			// Sets the amount of frames this minion has on its spritesheet
-			Main.projFrames[projectile.type] = 1;
+			Main.projFrames[Projectile.type] = 1;
 			// This is necessary for right-click targeting
-			ProjectileID.Sets.MinionTargettingFeature[projectile.type] = true;
+			ProjectileID.Sets.MinionTargettingFeature[Projectile.type] = true;
 
 			// These below are needed for a minion
 			// Denotes that this projectile is a pet or minion
-			Main.projPet[projectile.type] = true;
+			Main.projPet[Projectile.type] = true;
 			// This is needed so your minion can properly spawn when summoned and replaced when other minions are summoned
-			ProjectileID.Sets.MinionSacrificable[projectile.type] = true;
+			ProjectileID.Sets.MinionSacrificable[Projectile.type] = true;
 			// Don't mistake this with "if this is true, then it will automatically home". It is just for damage reduction for certain NPCs
-			ProjectileID.Sets.Homing[projectile.type] = true;
+			ProjectileID.Sets.CultistIsResistantTo[Projectile.type] = true;
 		}
 
-		public sealed override void SetDefaults()
-		{
-			projectile.width = 94;
-			projectile.height = 30;
+		public sealed override void SetDefaults() {
+			Projectile.width = 94;
+			Projectile.height = 30;
 			// Makes the minion go through tiles freely
-			projectile.tileCollide = false;
+			Projectile.tileCollide = false;
 
 			// These below are needed for a minion weapon
 			// Only controls if it deals damage to enemies on contact (more on that later)
-			projectile.friendly = true;
+			Projectile.friendly = true;
 			// Only determines the damage type
-			projectile.minion = true;
+			Projectile.minion = true;
 			// Amount of slots this minion occupies from the total minion slots available to the player (more on that later)
-			projectile.minionSlots = 1f;
+			Projectile.minionSlots = 1f;
 			// Needed so the minion doesn't despawn on collision with enemies or tiles
-			projectile.penetrate = -1;
+			Projectile.penetrate = -1;
 
 			random = new Random();
 		}
 
 		// Here you can decide if your minion breaks things like grass or pots
-		public override bool? CanCutTiles()
-		{
+		public override bool? CanCutTiles() {
 			return false;
 		}
 
 		// This is mandatory if your minion deals contact damage (further related stuff in AI() in the Movement region)
-		public override bool MinionContactDamage()
-		{
+		public override bool MinionContactDamage() {
 			return true;
 		}
 
-		public float Timer
-		{
-			get => projectile.ai[0];
-			set => projectile.ai[0] = value;
+		public float Timer {
+			get => Projectile.ai[0];
+			set => Projectile.ai[0] = value;
 		}
 
-		public override void AI()
-		{
-			Player player = Main.player[projectile.owner];
+		public override void AI() {
+			Player player = Main.player[Projectile.owner];
 
 			#region Active check
 			// This is the "active check", makes sure the minion is alive while the player is alive, and despawns if not
-			if (player.dead || !player.active)
-			{
+			if (player.dead || !player.active) {
 				player.ClearBuff(BuffType<ZenithStaffMinionBuff>());
 			}
-			if (player.HasBuff(BuffType<ZenithStaffMinionBuff>()))
-			{
-				projectile.timeLeft = 2;
+			if (player.HasBuff(BuffType<ZenithStaffMinionBuff>())) {
+				Projectile.timeLeft = 2;
 			}
 			#endregion
 
@@ -180,15 +157,14 @@ namespace ZenithGun.Items.Weapons.ZenithStaff
 			// All of this code below this line is adapted from Spazmamini code (ID 388, aiStyle 66)
 
 			// Teleport to player if distance is too big
-			Vector2 vectorToIdlePosition = idlePosition - projectile.Center;
+			Vector2 vectorToIdlePosition = idlePosition - Projectile.Center;
 			float distanceToIdlePosition = vectorToIdlePosition.Length();
-			if (Main.myPlayer == player.whoAmI && distanceToIdlePosition > 2000f)
-			{
+			if (Main.myPlayer == player.whoAmI && distanceToIdlePosition > 2000f) {
 				// Whenever you deal with non-regular events that change the behavior or position drastically, make sure to only run the code on the owner of the projectile,
 				// and then set netUpdate to true
-				projectile.position = idlePosition;
-				projectile.velocity *= 0.1f;
-				projectile.netUpdate = true;
+				Projectile.position = idlePosition;
+				Projectile.velocity *= 0.1f;
+				Projectile.netUpdate = true;
 			}
 
 			// If your minion is flying, you want to do this independently of any conditions
@@ -213,52 +189,46 @@ namespace ZenithGun.Items.Weapons.ZenithStaff
 			#region Find target
 			// Starting search distance
 			float distanceFromTarget = 700f;
-			Vector2 targetCenter = projectile.position;
+			Vector2 targetCenter = Projectile.position;
 			Vector2 targetCenterOffset = targetCenter;
 			bool foundTarget = false;
 
 			// This code is required if your minion weapon has the targeting feature
-			if (player.HasMinionAttackTargetNPC)
-			{
+			if (player.HasMinionAttackTargetNPC) {
 				NPC npc = Main.npc[player.MinionAttackTargetNPC];
-				float between = Vector2.Distance(npc.Center, projectile.Center);
+				float between = Vector2.Distance(npc.Center, Projectile.Center);
 				// Reasonable distance away so it doesn't target across multiple screens
-				if (between < 2000f)
-				{
+				if (between < 2000f) {
 					distanceFromTarget = between;
 					targetCenter = npc.Center;
-					if(theta == -1)
-						theta = random.NextDouble()*Math.PI*2;
+					if (theta == -1)
+						theta = random.NextDouble() * Math.PI * 2;
 					targetCenterOffset = targetCenter;
-					targetCenterOffset.X += 64f * (float)Math.Cos(theta);
-					targetCenterOffset.Y += 64f * (float)Math.Sin(theta);
+					targetCenterOffset.X += 64f * (float) Math.Cos(theta);
+					targetCenterOffset.Y += 64f * (float) Math.Sin(theta);
 					foundTarget = true;
 				}
 			}
-			if (!foundTarget)
-			{
+			if (!foundTarget) {
 				// This code is required either way, used for finding a target
-				for (int i = 0; i < Main.maxNPCs; i++)
-				{
+				for (int i = 0; i < Main.maxNPCs; i++) {
 					NPC npc = Main.npc[i];
-					if (npc.CanBeChasedBy())
-					{
-						float between = Vector2.Distance(npc.Center, projectile.Center);
-						bool closest = Vector2.Distance(projectile.Center, targetCenter) > between;
+					if (npc.CanBeChasedBy()) {
+						float between = Vector2.Distance(npc.Center, Projectile.Center);
+						bool closest = Vector2.Distance(Projectile.Center, targetCenter) > between;
 						bool inRange = between < distanceFromTarget;
 						//bool lineOfSight = Collision.CanHitLine(projectile.position, projectile.width, projectile.height, npc.position, npc.width, npc.height);
 						// Additional check for this specific minion behavior, otherwise it will stop attacking once it dashed through an enemy while flying though tiles afterwards
 						// The number depends on various parameters seen in the movement code below. Test different ones out until it works alright
 						//bool closeThroughWall = between < 100f;
-						if (((closest && inRange) || !foundTarget))
-						{
+						if (((closest && inRange) || !foundTarget)) {
 							distanceFromTarget = between;
 							targetCenter = npc.Center;
 							if (theta == -1)
 								theta = random.NextDouble() * Math.PI * 2;
 							targetCenterOffset = targetCenter;
-							targetCenterOffset.X += 200f * (float)Math.Cos(theta);
-							targetCenterOffset.Y += 200f * (float)Math.Sin(theta);
+							targetCenterOffset.X += 200f * (float) Math.Cos(theta);
+							targetCenterOffset.Y += 200f * (float) Math.Sin(theta);
 							foundTarget = true;
 						}
 					}
@@ -269,7 +239,7 @@ namespace ZenithGun.Items.Weapons.ZenithStaff
 			// friendly needs to be set to false so it doesn't damage things like target dummies while idling
 			// Both things depend on if it has a target or not, so it's just one assignment here
 			// You don't need this assignment if your minion is shooting things instead of dealing contact damage
-			projectile.friendly = foundTarget;
+			Projectile.friendly = foundTarget;
 			#endregion
 
 			#region Movement
@@ -278,64 +248,53 @@ namespace ZenithGun.Items.Weapons.ZenithStaff
 			float speed = 50f;
 			float inertia = 10f;
 
-			if(isSlashing)
-			{
+			if (isSlashing) {
 				Timer++;
-				if (Timer > 20)
-				{
-					projectile.velocity = Vector2.Zero;
-					theta += Math.PI + random.NextDouble()-0.5;
+				if (Timer > 20) {
+					Projectile.velocity = Vector2.Zero;
+					theta += Math.PI + random.NextDouble() - 0.5;
 					isSlashing = false;
 					isSpinning = false;
 					Timer = 0;
+				} else if (foundTarget) {
+					Projectile.velocity = (Projectile.velocity * (inertia - 1) + direction) / inertia;
 				}
-				else if(foundTarget)
-				{
-					projectile.velocity = (projectile.velocity * (inertia - 1) + direction) / inertia;
-				}
-			}
-			else if (foundTarget)
-			{
+			} else if (foundTarget) {
 				Timer++;
-				projectile.position += (targetCenterOffset - projectile.position) * new Vector2(0.05f, 0.05f);
-					
-				if (Timer > 90)
-				{
+				Projectile.position += (targetCenterOffset - Projectile.position) * new Vector2(0.05f, 0.05f);
+
+				if (Timer > 90) {
 					isSlashing = true;
 					isSpinning = random.NextDouble() > 0.5;
-					direction = targetCenter - projectile.Center;
+					direction = targetCenter - Projectile.Center;
 					direction.Normalize();
 					direction *= speed;
-					projectile.velocity = (projectile.velocity * (inertia - 1) + direction) / inertia;
+					Projectile.velocity = (Projectile.velocity * (inertia - 1) + direction) / inertia;
 					Timer = 0;
 				}
 
-			}
-			else
-			{
+			} else {
 				// Minion doesn't have a target: return to player and idle
-				projectile.position += (idlePosition - projectile.position) * new Vector2(0.05f, 0.05f);
-				projectile.velocity = Vector2.Zero;
+				Projectile.position += (idlePosition - Projectile.position) * new Vector2(0.05f, 0.05f);
+				Projectile.velocity = Vector2.Zero;
 				isSpinning = false;
 			}
 
-			projectile.friendly = isSlashing;
+			Projectile.friendly = isSlashing;
 			#endregion
 
 			#region Animation and visuals
 
-			if (isSpinning)
-			{
-				projectile.rotation += 1f;
+			if (isSpinning) {
+				Projectile.rotation += 1f;
+			} else if (!isSlashing) {
+				if (foundTarget) {
+					Projectile.rotation = (targetCenter - Projectile.Center).ToRotation();
+				} else {
+					Projectile.rotation = (float) Math.PI / 2f;
+				}
 			}
-			else if (!isSlashing)
-			{
-				if (foundTarget)
-					projectile.rotation = (targetCenter - projectile.Center).ToRotation();
-				else
-					projectile.rotation = (float)Math.PI / 2f;
-			}
-			
+
 			/*
 			// This is a simple "loop through all frames from top to bottom" animation
 			int frameSpeed = 5;
@@ -352,7 +311,7 @@ namespace ZenithGun.Items.Weapons.ZenithStaff
 			*/
 
 			// Some visuals here
-			Lighting.AddLight(projectile.Center, Color.White.ToVector3() * 0.78f);
+			Lighting.AddLight(Projectile.Center, Color.White.ToVector3() * 0.78f);
 			#endregion
 		}
 	}
